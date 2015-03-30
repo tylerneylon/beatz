3338924b266aec0e280aa943e1be3853808b09e3
Tyler Neylon <tylerneylon@gmail.com>
2015-03-30 16:34:22 -0700

    Start work on the sounds.load function.

--- c/sounds.m
+++ c/sounds.m
@@ -199,23 +199,68 @@ static void open_and_play_a_sound() {
 
 }
 
+// Load and prime the file.
+// TODO Be sure that we do something appropriate if there's an error.
+static AudioQueueRef load_queue_for_file(const char *filename) {
+  NSString *fileName = [NSString stringWithUTF8String:filename];
+  NSURL *fileURL = [NSURL URLWithString:fileName];
+  
+  ExtAudioFileRef audioFile;
+  OSStatus status = ExtAudioFileOpenURL((__bridge CFURLRef)fileURL, &audioFile);
+  print_err_if_bad(status, @"ExtAudioFileOpenURL");
+  
+  AudioStreamBasicDescription audioDesc;
+  UInt32 audioDescSize = sizeof(audioDesc);
+  
+  status = ExtAudioFileGetProperty(audioFile,
+                                   kExtAudioFileProperty_FileDataFormat,
+                                   &audioDescSize,
+                                   &audioDesc);
+  print_err_if_bad(status, @"ExtAudioFileGetProperty");
+  
+  AudioQueueRef audioQueue;
+  
+  status = AudioQueueNewOutput(&audioDesc,
+                               file_play_callback,
+                               audioFile,  // user data
+                               NULL, /* CFRunLoopGetCurrent(), */
+                               kCFRunLoopCommonModes,
+                               0,     // reserved flags; must be 0
+                               &audioQueue);
+  print_err_if_bad(status, @"AudioQueueNewOutput");
+  
+  for (int i = 0; i < 2; ++i) {
+    UInt32 bufferByteSize = 4 * 1024;
+    AudioQueueBufferRef buffer;
+    status = AudioQueueAllocateBuffer(audioQueue,
+                                      bufferByteSize,
+                                      &buffer);
+    print_err_if_bad(status, @"AudioQueueAllocateBuffer");
+    
+    file_play_callback(audioFile,  // user data
+                       audioQueue,
+                       buffer);
+  }
+  
+  status = AudioQueueAddPropertyListener(audioQueue,
+                                         kAudioQueueProperty_IsRunning,
+                                         file_ended_callback,
+                                         NULL);  // user data
+  print_err_if_bad(status, @"AudioQueueAddPropertyListener");
+  
+  //status = AudioQueueStart(audioQueue, NULL);  // NULL --> start as soon as possible
+  //print_err_if_bad(status, @"AudioQueueStart");
+
+  return audioQueue;
+}
+
 void *sounds_thread(void *arg) {
   @autoreleasepool {
-    
-    //open_an_audio_file();
-    
-    //play_a_sound();
-    
-    open_and_play_a_sound();
-    
-    //sleep(10);
-    
-    do {
+    while (1) {
       CFRunLoopRunInMode(kCFRunLoopDefaultMode,
                          0.25,     // run time
                          false);   // run for full run time
-    } while (is_playing);
-    
+    }
   }
   return 0;
 }
@@ -232,11 +277,37 @@ static int sayhi(lua_State *L) {
   return 0;
 }
 
+// TODO cleanup
+
+typedef struct {
+  AudioQueueRef queue;
+} Audio;
+
+// Function to load an audio file.
+static int loadfile(lua_State *L) {
+  // TEMP TODO Remove debug stuff and clean up this fn.
+  printf("start of %s\n", __FUNCTION__);
+
+  // TODO Test behavior when no param or a nonstring is given.
+  const char *filename = luaL_checkstring(L, 1);
+
+  printf("Got the filename '%s'\n", filename);
+
+  Audio *audio = lua_newuserdata(L, sizeof(Audio));
+
+  audio->queue = load_queue_for_file(filename);
+
+  printf("end of %s\n", __FUNCTION__);
+
+  return 1;
+}
+
 
 // Data for the exported sounds table.
 
 static const struct luaL_Reg sounds[] = {
   {"sayhi", sayhi},
+  {"load", loadfile},
   {NULL, NULL}
 };
 
