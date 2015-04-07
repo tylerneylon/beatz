73558a3254c1362ac91fe2561961a0f8b8d68ebe
Tyler Neylon <tylerneylon@gmail.com>
2015-04-07 08:50:32 -0700

    Close file immediately after reading it.

--- c/sounds.m
+++ c/sounds.m
@@ -20,13 +20,14 @@ typedef struct {
   AudioQueueRef   queue;  // TODO Is this used?
   // TODO Either remove audioFile by using it entirely within read_entire_file
   //      or garbage collect it properly. Either way, make sure it's closed.
-  ExtAudioFileRef audioFile;
-  char *          bytes;
-  size_t          num_bytes;
-  char *          cursor;
-  int             index;  // Index in the sounds_mt.playing table.
-  lua_State *     L;      // This is useful for callback user data.
-  int             is_running;
+  ExtAudioFileRef             audioFile;
+  char *                      bytes;
+  size_t                      num_bytes;
+  char *                      cursor;
+  int                         index;  // Index in the sounds_mt.playing table.
+  lua_State *                 L;      // This is useful for callback user data.
+  int                         is_running;
+  AudioStreamBasicDescription audioDesc;
 } Sound;
 
 
@@ -78,6 +79,16 @@ static void read_entire_file(const char *filename, Sound *sound) {
   sound->bytes     = full_ptr;
   sound->num_bytes = totalFrames * bytesPerFrame;
   sound->cursor    = full_ptr;
+
+  UInt32 audioDescSize = sizeof(sound->audioDesc);
+  status = ExtAudioFileGetProperty(sound->audioFile,
+                                   kExtAudioFileProperty_FileDataFormat,
+                                   &audioDescSize,
+                                   &sound->audioDesc);
+  print_err_if_bad(status, @"ExtAudioFileGetProperty");
+
+  status = ExtAudioFileDispose(sound->audioFile);
+  print_err_if_bad(status, @"ExtAudioFileDispose");
 }
 
 void file_play_callback(void *userData, AudioQueueRef inAQ, AudioQueueBufferRef buffer) {
@@ -195,61 +206,6 @@ void running_status_changed(void *userData, AudioQueueRef audioQueue,
   }
 }
 
-// Load and prime the file.
-// TODO Be sure that we do something appropriate if there's an error.
-static AudioQueueRef load_queue_for_file(const char *filename) {
-  NSString *fileName = [NSString stringWithUTF8String:filename];
-  NSURL *fileURL = [NSURL URLWithString:fileName];
-  
-  ExtAudioFileRef audioFile;
-  OSStatus status = ExtAudioFileOpenURL((__bridge CFURLRef)fileURL, &audioFile);
-  print_err_if_bad(status, @"ExtAudioFileOpenURL");
-  
-  AudioStreamBasicDescription audioDesc;
-  UInt32 audioDescSize = sizeof(audioDesc);
-  
-  status = ExtAudioFileGetProperty(audioFile,
-                                   kExtAudioFileProperty_FileDataFormat,
-                                   &audioDescSize,
-                                   &audioDesc);
-  print_err_if_bad(status, @"ExtAudioFileGetProperty");
-  
-  AudioQueueRef audioQueue;
-  
-  status = AudioQueueNewOutput(&audioDesc,
-                               file_play_callback,
-                               audioFile,  // user data
-                               NULL, /* CFRunLoopGetCurrent(), */
-                               kCFRunLoopCommonModes,
-                               0,     // reserved flags; must be 0
-                               &audioQueue);
-  print_err_if_bad(status, @"AudioQueueNewOutput");
-  
-  for (int i = 0; i < 2; ++i) {
-    UInt32 bufferByteSize = 4 * 1024;
-    AudioQueueBufferRef buffer;
-    status = AudioQueueAllocateBuffer(audioQueue,
-                                      bufferByteSize,
-                                      &buffer);
-    print_err_if_bad(status, @"AudioQueueAllocateBuffer");
-    
-    file_play_callback(audioFile,  // user data
-                       audioQueue,
-                       buffer);
-  }
-  
-  status = AudioQueueAddPropertyListener(audioQueue,
-                                         kAudioQueueProperty_IsRunning,
-                                         running_status_changed,
-                                         NULL);  // user data
-  print_err_if_bad(status, @"AudioQueueAddPropertyListener");
-  
-  //status = AudioQueueStart(audioQueue, NULL);  // NULL --> start as soon as possible
-  //print_err_if_bad(status, @"AudioQueueStart");
-
-  return audioQueue;
-}
-
 void *sounds_thread(void *arg) {
   @autoreleasepool {
     while (1) {
@@ -286,24 +242,15 @@ static int play_sound(lua_State *L) {
   // Don't do anything if the sound is already playing.
   if (sound->is_running) return 0;
 
-  AudioStreamBasicDescription audioDesc;
-  UInt32 audioDescSize = sizeof(audioDesc);
-  
-  OSStatus status = ExtAudioFileGetProperty(sound->audioFile,
-                                            kExtAudioFileProperty_FileDataFormat,
-                                            &audioDescSize,
-                                            &audioDesc);
-  print_err_if_bad(status, @"ExtAudioFileGetProperty");
-  
   AudioQueueRef audioQueue;
   
-  status = AudioQueueNewOutput(&audioDesc,
-                               sound_play_callback,
-                               sound,  // user data
-                               NULL, /* CFRunLoopGetCurrent(), */
-                               kCFRunLoopCommonModes,
-                               0,     // reserved flags; must be 0
-                               &audioQueue);
+  OSStatus status = AudioQueueNewOutput(&sound->audioDesc,
+                                        sound_play_callback,
+                                        sound,  // user data
+                                        NULL, /* CFRunLoopGetCurrent(), */
+                                        kCFRunLoopCommonModes,
+                                        0,     // reserved flags; must be 0
+                                        &audioQueue);
   print_err_if_bad(status, @"AudioQueueNewOutput");
   
   for (int i = 0; i < 2; ++i) {
