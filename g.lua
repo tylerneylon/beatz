438721c3897a53bfb957e52096f83db8965a8933
Tyler Neylon <tylerneylon@gmail.com>
2015-04-12 12:22:40 -0700

    Checkpoint while debugging repeat playback.
    I had to do some testing to understand how Audio Queues handle buffers
    between a stop and the next play.

---
+++ g.lua
@@ -0,0 +1,5 @@
+sounds = require 'sounds'
+a = sounds.load('instruments/letters/a.wav')
+b = sounds.load('instruments/letters/b.wav')
+c = sounds.load('instruments/letters/c.wav')
+d = sounds.load('instruments/letters/d.wav')
